1186bbb78f93ad9c290ff3565382c7eeb680d16c
Samir739 <sgiri@unm.edu>
2024-09-23 14:32:16 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your Name: ");
+        String userName = Console.ReadLine();
+        Console.WriteLine("Hello, " + userName + "! Welcome to the world of C#. Let the adventure begin!");
+    }
+}
+
+
