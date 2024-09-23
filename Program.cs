6a06771a0c72c9c1ae0a10848024a1c264051512
Samir739 <sgiri@unm.edu>
2024-09-23 14:38:45 -0600

    Add files via upload
    This is a Fibonacci Game coded with C#.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,31 @@
-﻿class Program
+﻿class Fibonacci
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter your Name: ");
-        String userName = Console.ReadLine();//Take the name from User
-        Console.WriteLine("Hello, " + userName + "! Welcome to the world of C#. Let the adventure begin!");
+        Console.WriteLine("Enter any number greater then 0: ");
+        int firstnumber = Convert.ToInt32(Console.ReadLine());
+        long x = 0;
+        long y = 1;
+        for (int i = 0; i <= firstnumber-1; i++)
+        {
+            Console.WriteLine($"{Ordinal(i+1)} number: " + x);
+            long a = x;
+            x = y;
+            y = a + y;
+        }
+        static string Ordinal(int number)
+        {
+            if (number % 100 >= 11 && number % 100 <= 13)
+                return number + "th";
+
+            switch (number % 10)
+            {
+                case 1: return number + "st";
+                case 2: return number + "nd";
+                case 3: return number + "rd";
+                default: return number + "th";
+            }
+        }
     }
 }
-
 
