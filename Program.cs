1c3b8a491badc66c02940ca4816b3ae3849f23af
Samir739 <sgiri@unm.edu>
2024-09-23 14:34:27 -0600

    Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
     static void Main(string[] args)
     {
         Console.Write("Enter your Name: ");
-        String userName = Console.ReadLine();
+        String userName = Console.ReadLine();//Take the name from User
         Console.WriteLine("Hello, " + userName + "! Welcome to the world of C#. Let the adventure begin!");
     }
 }
